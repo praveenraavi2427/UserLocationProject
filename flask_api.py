eca6322bdb75479f58c63ce4f1df980f0fec94ee
praveenraavi2427 <89962323+praveenraavi2427@users.noreply.github.com>
2021-09-02 13:08:15 +0530

    Add files via upload

---
+++ flask_api.py
@@ -0,0 +1,114 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        data = pd.read_csv('users_account.csv')  # read local CSV
+        data = data.to_dict()  # convert dataframe to dict
+        return {'data': data}, 200  # return data and 200 OK
+
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('users_account.csv')
+
+        if args['userId'] in list(data['userId']):
+            return {
+                'message': f"'{args['userId']}' already exists."
+            }, 409
+        else:
+            # create new dataframe containing new values
+            new_data = pd.DataFrame({
+                'userId': [args['userId']],
+                'name': [args['name']],
+                'city': [args['city']]
+            })
+            # add the newly provided values
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv('users.csv', index=False)  # save back to CSV
+            return {'data': data.to_dict()}, 200  # return data with 200 OK
+
+    def put(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('users_account.csv')
+        
+        if args['userId'] in list(data['userId']):
+            # evaluate strings of lists to lists !!! never put something like this in prod
+            data['locations'] = data['locations'].apply(
+                lambda x: ast.literal_eval(x)
+            )
+            # select our user
+            user_data = data[data['userId'] == args['userId']]
+
+            # update user's locations
+            user_data['locations'] = user_data['locations'].values[0] \
+                .append(args['location'])
+            
+            # save back to CSV
+            data.to_csv('users_account.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+
+        else:
+            # otherwise the userId does not exist
+            return {
+                'message': f"'{args['userId']}' user not found."
+            }, 404
+
+                    
+class Locations(Resource):
+    def get(self):
+        data = pd.read_csv('locations_bangalore.csv')  # read local CSV
+        return {'data': data.to_dict()}, 200  # return data dict and 200 OK
+    
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize parser
+        parser.add_argument('locationId', required=True, type=int)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('rating', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+        
+        # read our CSV
+        data = pd.read_csv('locations.csv')
+    
+        # check if location already exists
+        if args['locationId'] in list(data['locationId']):
+            # if locationId already exists, return 401 unauthorized
+            return {
+                'message': f"'{args['locationId']}' already exists."
+            }, 409
+        else:
+            # otherwise, we can add the new location record
+            # create new dataframe containing new values
+            new_data = pd.DataFrame({
+                'locationId': [args['locationId']],
+                'name': [args['name']],
+                'rating': [args['rating']]
+            })
+            # add the newly provided values
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv('locations_bangalore.csv', index=False)  # save back to CSV
+            return {'data': data.to_dict()}, 200  # return data with 200 OK
+    
+
+
+api.add_resource(Users, '/users')  # add endpoints
+api.add_resource(Locations, '/locations')
+
+if __name__ == '__main__':
+    app.run()
